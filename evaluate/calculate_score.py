894d8c16c6c77e81ea86899a8392c09d5a1b33b3
kevin996231 <b05902128@ntu.edu.tw>
2021-12-01 07:48:14 -0500

    Add score calculation

---
+++ evaluate/calculate_score.py
@@ -0,0 +1,41 @@
+from pyserini.search import SimpleSearcher
+from sklearn.metrics import ndcg_score, dcg_score
+import pandas as pd
+import numpy as np
+import math
+
+
+df = pd.read_csv('./Annotations.csv')
+queries = pd.unique(df['Query'].dropna())
+searcher = SimpleSearcher('../indexes/')
+
+log = []
+for i in range(1,11):
+    log.append(1/(math.log(i+1)/math.log(2)))
+log = np.array(log)
+
+total_ndcg = 0
+for query in queries:
+    hits = searcher.search(query)
+    true_label = np.append(df.loc[df['Query'] == query]['Score'].dropna().to_numpy(), [0]*10)
+    true_label = np.sort(true_label)[-1:-11:-1]
+    idcg = np.dot(true_label, log)
+
+    output_label = [0]*10
+    for i in range(min(10, len(hits))):
+        score = df.loc[df['Query'] == query].loc[df['Entry'] == hits[i].docid]
+        if not score.empty:
+            output_label[i] = score.iloc[0]['Score']
+    output_label = np.array(output_label)
+
+    dcg = np.dot(output_label, log)
+    ndcg = dcg/idcg
+    total_ndcg += ndcg
+    print(f'Query: {query}')
+    print(f'DCG: {dcg}')
+    print(f'iDCG: {idcg}')
+    print(f'NDCG: {ndcg}')
+    print(true_label)
+    print(output_label)
+print(f'Average NDCG: {total_ndcg/len(queries)}')
+
